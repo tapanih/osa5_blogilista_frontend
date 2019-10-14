bb831836e6d9b68c94afd87575199e94f621796e
Tapani Honkanen <tapani.honkanen@hotmail.com>
2019-10-14 16:40:52 +0300

    Lisätty yksinkertainen kirjautumisominaisuus

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import blogService from './services/blogs'
+import loginService from './services/login'
 import Blog from './components/Blog'
 
 const App = () => {
   const [ blogs, setBlogs ] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService
@@ -13,12 +17,60 @@ const App = () => {
       })
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null
+        ? loginForm()
+        :
+        <div>
+          <h2>blogs</h2>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      }
     </div>
   )
 }
