395417fa0d23c400969c2e15b4244bb4668b8e7d
Tapani Honkanen <tapani.honkanen@hotmail.com>
2019-10-15 15:39:29 +0300

    Laajennettu blogien listausta siten, että klikkaamalla blogin nimeä, sen täydelliset tiedot aukeavat

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={toggleVisibility}>
+        {blog.title} {blog.author}<br/>
+        <div style={showWhenVisible}>
+          <a href={blog.url}>{blog.url}</a><br/>
+          {blog.likes} <button onClick={() => console.log('klikattu')}>like</button><br/>
+          Added by {blog.user.name}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
