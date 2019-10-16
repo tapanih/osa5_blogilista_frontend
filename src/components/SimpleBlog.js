8deab105a2bfffe27f07800fd7f4d89e20066d79
Tapani Honkanen <tapani.honkanen@hotmail.com>
2019-10-16 19:03:53 +0300

    Refaktoroitu testejä

--- src/components/SimpleBlog.js
+++ src/components/SimpleBlog.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const SimpleBlog = ({ blog, onClick }) => (
-  <div>
+  <div className='simpleBlog'>
     <div>
       {blog.title} {blog.author}
     </div>
--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -1,33 +1,9 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent, cleanup } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
-afterEach(cleanup)
-
-test('renders title, authors and likes', () => {
-  const blog = {
-    title: 'React Patterns',
-    author: 'Michael Chan',
-    likes: 7
-  }
-
-  const component = render(
-    <SimpleBlog blog={blog} onClick={() => console.log('click')} />
-  )
-
-  expect(component.container).toHaveTextContent(
-    'React Patterns'
-  )
-  expect(component.container).toHaveTextContent(
-    'Michael Chan'
-  )
-  expect(component.container).toHaveTextContent(
-    '7'
-  )
-})
-
-test('calls handler twice when the like button is pressed twice', () => {
+describe('<SimpleBlog/>', () => {
   const blog = {
     title: 'React Patterns',
     author: 'Michael Chan',
@@ -35,15 +11,28 @@ test('calls handler twice when the like button is pressed twice', () => {
   }
 
   const mockHandler = jest.fn()
+  let component
 
-  const component = render(
-    <SimpleBlog blog={blog} onClick={mockHandler} />
-  )
+  beforeEach(() => {
+    component = render(
+      <SimpleBlog blog={blog} onClick={mockHandler} />
+    )
+  })
 
-  const button = component.container.querySelector('.like')
+  test('renders title, authors and likes', () => {
+    const div = component.container.querySelector('.simpleBlog')
+    expect(div).toHaveTextContent('React Patterns')
+    expect(div).toHaveTextContent('Michael Chan')
+    expect(div).toHaveTextContent('7')
+  })
 
-  fireEvent.click(button)
-  fireEvent.click(button)
+  test('calls handler twice when the like button is pressed twice', () => {
 
-  expect(mockHandler.mock.calls.length).toBe(2)
+    const button = component.container.querySelector('.like')
+
+    fireEvent.click(button)
+    fireEvent.click(button)
+
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
 })
