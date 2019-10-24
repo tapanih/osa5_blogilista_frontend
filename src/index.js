910e8246a7157c780a594ed7037cb4f456e131e6
Tapani Honkanen <tapani.honkanen@hotmail.com>
2019-10-24 16:34:04 +0300

    Siirretty staten määrittely omaan tiedostoon

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
 import App from './App'
-import notificationReducer from './reducers/notificationReducer'
-
-const reducer = combineReducers({
-  notification: notificationReducer
-})
-
-const store = createStore(reducer, applyMiddleware(thunk))
+import store from './store'
 
 const render = () => {
   ReactDOM.render(
